cee2fa012044e091f6e119d7d6e586d30b0d01aa
KEVIN\Kevin Duong <1999duong.kevin@gmail.com>
2019-05-13 23:53:11 +1000

    Free drawing canvas board added with single colour option

--- user-interface/src/User_interface_Main.java
+++ user-interface/src/User_interface_Main.java
@@ -1,24 +1,48 @@
 import javafx.application.Application;
+
 import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+
+import javafx.event.EventHandler;
+
+import javafx.stage.Stage;
+import javafx.stage.FileChooser;
+
 
 import java.io.File;
 
 public class User_interface_Main extends Application {
 
-    /**
-     * 
-     *
-      * @param args
-     */
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void initDraw(GraphicsContext gc){
+        double canvasWidth = gc.getCanvas().getWidth();
+        double canvasHeight = gc.getCanvas().getHeight();
+
+        gc.setFill(Color.LIGHTGRAY);
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(3);
+
+        gc.fill();
+        gc.strokeRect(
+                0,              //x of the upper left corner
+                0,              //y of the upper left corner
+                canvasWidth,    //width of the rectangle
+                canvasHeight);  //height of the rectangle
+
+        gc.setFill(Color.RED);
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(1);
+
     }
 
     @Override
@@ -57,7 +81,6 @@ public class User_interface_Main extends Application {
         menuFile.getItems().add(menuItemOpen);
 
         // Listeners for Menu items
-
         menuItemNew.setOnAction(e -> {
             System.out.println("Menu Item 'New' Selected");
         });
@@ -70,60 +93,51 @@ public class User_interface_Main extends Application {
         });
 
         // Create drawing canvas
+        Canvas canvas = new Canvas(640, 400);
+        final GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+        initDraw(graphicsContext);
 
-        // Create the Canvas
-        Canvas canvas = new Canvas(200, 200);
-        // Set the width of the Canvas
-        canvas.setWidth(200);
-        // Set the height of the Canvas
-        canvas.setHeight(200);
+        canvas.addEventHandler(MouseEvent.MOUSE_PRESSED,
+                new EventHandler<MouseEvent>(){
 
-        // Get the graphics context of the canvas
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+                    @Override
+                    public void handle(MouseEvent event) {
+                        graphicsContext.beginPath();
+                        graphicsContext.moveTo(event.getX(), event.getY());
+                        graphicsContext.stroke();
+                    }
+                });
 
-        // Draw a Text
-        gc.strokeText("Hello Canvas", 150, 100);
+        canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED,
+                new EventHandler<MouseEvent>(){
+
+                    @Override
+                    public void handle(MouseEvent event) {
+                        graphicsContext.lineTo(event.getX(), event.getY());
+                        graphicsContext.stroke();
+                    }
+                });
+
+        canvas.addEventHandler(MouseEvent.MOUSE_RELEASED,
+                new EventHandler<MouseEvent>(){
+
+                    @Override
+                    public void handle(MouseEvent event) {
+
+                    }
+                });
 
         // Create the Pane
         Pane root = new Pane();
-        // Set the Style-properties of the Pane
-        root.setStyle("-fx-padding: 10;" +
-                "-fx-border-style: solid inside;" +
-                "-fx-border-width: 2;" +
-                "-fx-border-insets: 5;" +
-                "-fx-border-radius: 5;" +
-                "-fx-border-color: blue;");
-
         // Add the Canvas to the Pane
         root.getChildren().add(canvas);
 
-        // Create the Scene
-        //Scene scene = new Scene(root);
-        // Add the Scene to the Stage
-        //primaryStage.setScene(scene);
-        // Set the Title of the Stage
-        //primaryStage.setTitle("Creation of a Canvas");
-        // Display the Stage
-        //primaryStage.show();
-
         // Creating the Scene
-
-        VBox vBox = new VBox(menuBar);
+        VBox vBox = new VBox(menuBar, root);
 
         Scene scene = new Scene(vBox, 640, 480);
 
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
-
-//    public void start(Stage primaryStage) throws Exception{
-//        String javaVersion = System.getProperty("java.version");
-//        String javafxVersion = System.getProperty("javafx.version");
-//        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-//        Scene scene = new Scene(new StackPane(l), 640, 480);
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
-//    }
-
 }
