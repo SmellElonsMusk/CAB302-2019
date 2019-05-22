3f7bae15081eda7a64d09641dd324e0fb7f655ed
Waldo Fouche <waldo.fouche@connect.qut.edu.au>
2019-05-22 15:58:41 +1000

    Added more event handlers

--- src/userInterface/Controller.java
+++ src/userInterface/Controller.java
@@ -2,23 +2,38 @@ package userInterface;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.control.Label;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.input.ZoomEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.stage.Window;
+import java.io.File;
 
 import backend.*;
 
-import java.io.File;
-
+/**
+ * @Author: Waldo Fouche, Kevin Doung
+ */
 public class Controller {
 
+    @FXML private Canvas canvas;
+    private GraphicsContext gc;
+
+    /**
+     *
+     * @param actionEvent
+     */
     @FXML
     public void clickFileNew(ActionEvent actionEvent) {
         System.out.println("Clicked: New");
     }
 
+    /**
+     * Event handler for click action on the File -> open menu item
+     * @param actionEvent
+     */
     @FXML
     protected void clickFileOpen(ActionEvent actionEvent) {
         FileChooser chooser = new FileChooser();
@@ -28,27 +43,71 @@ public class Controller {
         );
 
         File file = chooser.showOpenDialog(new Stage());
-
         System.out.println("Clicked: Open");
 
     }
 
+    /**
+     *
+     * @param actionEvent
+     */
     @FXML
     protected void clickSave(ActionEvent actionEvent) {
         System.out.println("Clicked: Save");
     }
 
+    /**
+     *
+     * @param actionEvent
+     */
     @FXML
     protected void clickSaveAs(ActionEvent actionEvent) {
         System.out.println("Clicked: Save as");
     }
 
+    /**
+     *
+     * @param actionEvent
+     */
     @FXML
     protected void clickAbout(ActionEvent actionEvent) {
         System.out.println("Clicked: about");
     }
 
+    @FXML
+    public void canvasMouseClick(MouseEvent mouseEvent) {
+        System.out.println("Mouse Clicked");
 
+    }
+
+    @FXML
+    public void canvasMouseDrag(MouseEvent mouseEvent) {
+        System.out.println("Mouse drag");
+    }
+
+    @FXML
+    public void canvasMouseScrollStart(ScrollEvent scrollEvent) {
+        System.out.println("Mouse Scroll Start");
+    }
+
+    @FXML
+    public void canvasMouseScrollStop(ScrollEvent scrollEvent) {
+        System.out.println("Mouse Scroll Stop");
+    }
+
+    @FXML
+    public void canvasZoom(ZoomEvent zoomEvent) {
+    }
+
+    @FXML
+    public void canvasZoomStopped(ZoomEvent zoomEvent) {
+        System.out.println("Mouse zoom stopped");
+    }
+
+    @FXML
+    public void canvasZoomStarted(ZoomEvent zoomEvent) {
+        System.out.println("Mouse zoom start");
+    }
 }
 
 
