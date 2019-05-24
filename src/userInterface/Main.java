6fcba700d8d6dd7316d0c3afd49b81e90e351ff2
WaldoF <waldo.fouche@connect.qut.edu.au>
2019-05-24 22:02:31 +1000

    Cleaned up

--- src/userInterface/Main.java
+++ src/userInterface/Main.java
@@ -8,11 +8,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import backend.*;
 
+import java.io.IOException;
+
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
         launch(args);
+
     }
 
     @Override
