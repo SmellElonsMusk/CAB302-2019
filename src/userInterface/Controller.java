62132f3ade4149797bc05760de21492e37499ac6
KEVIN\Kevin Duong <1999duong.kevin@gmail.com>
2019-05-26 18:48:38 +1000

    Improved loading file and added file titles to window

--- src/userInterface/Controller.java
+++ src/userInterface/Controller.java
@@ -85,7 +85,7 @@ public class Controller {
             scene.getStylesheets().add("userInterface/menuBarStylesheet.css");
 
             Stage stage = new Stage();
-            stage.setTitle("Vec Draw");
+            stage.setTitle("Vec Draw - untitled");
             stage.setScene(scene);
             stage.show();
 
@@ -105,42 +105,56 @@ public class Controller {
     @FXML
     protected void clickFileOpen(ActionEvent actionEvent) throws IOException {
         FileChooser chooser = new FileChooser();
+
         chooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("VEC Files", "*.vec")
         );
-
-        // Show file Dialog
-        File file_path = chooser.showOpenDialog(new Stage());
-
-        // File directory address
-        System.out.println(file_path);
-
-        // Gets only filename
-        String filename = new File(file_path.toString()).getName();
-        System.out.println(filename);
-
-        // Reading file...
-        try {
-            FileReader fr = new FileReader(file_path);
-            BufferedReader br = new BufferedReader(fr);
-
-            String str;
-            while ((str = br.readLine()) != null) {
-                System.out.println(str + "\n");
-            }
-
-            br.close();
-        } catch (IOException e) {
-            System.out.println("File not found");
-        }
-
 
         /*
             If the user successfully finds a file, then a new window will open with the selected image
             Otherwise if the user cancels or exits the dialog, do not open a new window
          */
 
+        File file_path = chooser.showOpenDialog(null);
 
+        if (file_path != null) {
+
+            try {
+                // Gets only filename
+                String filename = new File(file_path.toString()).getName();
+
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("ui_layout.fxml"));
+
+                Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+                scene.getStylesheets().add("userInterface/stylesheet.css");
+                scene.getStylesheets().add("userInterface/menuBarStylesheet.css");
+
+                Stage stage = new Stage();
+                stage.setTitle("Vec Draw - " + filename);
+                stage.setScene(scene);
+                stage.show();
+
+            } catch (IOException e) {
+                Logger logger = Logger.getLogger(getClass().getName());
+                logger.log(Level.SEVERE, "Failed to create new Window.", e);
+            }
+
+            // Reading file...
+            try {
+                FileReader fr = new FileReader(file_path);
+                BufferedReader br = new BufferedReader(fr);
+
+                String str;
+                while ((str = br.readLine()) != null) {
+                    System.out.println(str + "\n");
+                }
+
+                br.close();
+            } catch (IOException e) {
+                System.out.println("File not found");
+            }
+        }
     }
 
     /**
