20001e81810b16850b3a9465868107035720f7fb
WaldoF <waldo.fouche@connect.qut.edu.au>
2019-05-31 00:06:45 +1000

    Modified: Simplified Classes

--- src/backend/DisableButtons.java
+++ src/backend/DisableButtons.java
@@ -9,18 +9,7 @@ import javafx.scene.control.ToggleButton;
  **/
 
 public class DisableButtons {
-
-    ToggleButton b1;
-    ToggleButton b2;
-    ToggleButton b3;
-    ToggleButton b4;
-
     public DisableButtons(ToggleButton clickedButton, ToggleButton b1,ToggleButton b2,ToggleButton b3,ToggleButton b4) {
-
-        this.b1 = b1;
-        this.b2 = b2;
-        this.b3 = b3;
-        this.b4 = b4;
 
         if (clickedButton.isSelected()) {
             b1.setDisable(true);
--- src/backend/ReEnableButtons.java
+++ src/backend/ReEnableButtons.java
@@ -9,18 +9,7 @@ import javafx.scene.control.ToggleButton;
  **/
 
 public class ReEnableButtons {
-    ToggleButton b1;
-    ToggleButton b2;
-    ToggleButton b3;
-    ToggleButton b4;
-
     public ReEnableButtons(ToggleButton clickedButton, ToggleButton b1, ToggleButton b2, ToggleButton b3, ToggleButton b4) {
-
-        this.b1 = b1;
-        this.b2 = b2;
-        this.b3 = b3;
-        this.b4 = b4;
-
         b1.setDisable(false);
         b2.setDisable(false);
         b3.setDisable(false);
