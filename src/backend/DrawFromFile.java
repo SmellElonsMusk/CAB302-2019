a01f51c974584fdc1727f263aa3aec53d8af120a
Waldo Fouche <waldo.fouche@connect.qut.edu.au>
2019-06-01 03:43:18 +1000

    Added: Seperated draw function out of DrawFromFile, the file now only loads the text file, then offloads to the new Draw Function

--- src/backend/DrawFromFile.java
+++ src/backend/DrawFromFile.java
@@ -20,16 +20,7 @@ import java.util.regex.Pattern;
  * @Author Waldo Fouche, n995095
  */
 public class DrawFromFile {
-
     private ArrayList<String> line;
-    private ArrayList<Double> myDoubles;
-
-    private String hex[];
-
-    private Matcher doubles;
-
-    String regexDoubles = "[-+]?\\d*\\.?\\d+([eE][-+]?\\d+)?";
-
     public DrawFromFile(Canvas canvas, File file) {
         canvas.getGraphicsContext2D().setFill(Color.TRANSPARENT); // removes any fill command if selected
         canvas.getGraphicsContext2D().setStroke(Color.BLACK); // Resets Pen Tool to Black if currently another colour
@@ -41,113 +32,7 @@ public class DrawFromFile {
             this.line = new ArrayList<String>();
             while ((str = br.readLine()) != null) {
                 this.line.add(str);
-
-                // Check for colour
-                if (str.contains("FILL") ) {
-                    hex = str.split("\\s+");
-
-                    canvas.getGraphicsContext2D().setFill(Color.valueOf(hex[1]));
-
-                } else if (str.contains("PEN")) {
-                    hex = str.split("\\s+");
-
-                    canvas.getGraphicsContext2D().setFill(Color.TRANSPARENT); // removes fill
-                    canvas.getGraphicsContext2D().setStroke(Color.valueOf(hex[1]));
-
-
-                }
-
-                // TODO: Detect FILL OFF
-                // If FILL is OFF
-                if (str.contains("OFF")) {
-                    hex[1] = "#000000"; // White
-                    canvas.getGraphicsContext2D().setStroke(Color.valueOf(hex[1]));
-                    canvas.getGraphicsContext2D().setFill(Color.TRANSPARENT); // removes fill
-            }
-
-                // Check for drawing commands
-                if (str.contains("LINE") ) {
-                    // Scans for Doubles
-                    myDoubles = new ArrayList<Double>();
-                    doubles = Pattern.compile(regexDoubles).matcher(str);
-
-                    while (doubles.find()) {
-                        double element = Double.parseDouble(doubles.group());
-                        myDoubles.add(element);
-                    }
-
-                    Double x1 = myDoubles.get(0) * canvas.getWidth();
-                    Double y1 = myDoubles.get(1) * canvas.getHeight();
-                    Double x2 = myDoubles.get(2) * canvas.getWidth();
-                    Double y2 = myDoubles.get(3) * canvas.getHeight();
-
-                    canvas.getGraphicsContext2D().strokeLine(x1,y1,x2,y2);
-
-                } else if (str.contains("PLOT")) {
-
-                    // Scans for Doubles
-                    myDoubles = new ArrayList<Double>();
-                    doubles = Pattern.compile(regexDoubles).matcher(str);
-
-                    while (doubles.find()) {
-                        double element = Double.parseDouble(doubles.group());
-                        myDoubles.add(element);
-                    }
-
-                    Double x1 = myDoubles.get(0)*canvas.getWidth();
-                    Double y1 = myDoubles.get(1)*canvas.getWidth();
-
-
-                    canvas.getGraphicsContext2D().fillRoundRect(x1,y1,1,1,1,1);
-                  
-                } else if (str.contains("RECTANGLE")) {
-
-                    // Scans for Doubles
-                    myDoubles = new ArrayList<Double>();
-                    doubles = Pattern.compile(regexDoubles).matcher(str);
-
-                    while (doubles.find()) {
-                        double element = Double.parseDouble(doubles.group());
-                        myDoubles.add(element);
-                    }
-
-                    Double x1 = myDoubles.get(0)*canvas.getWidth();
-                    Double y1 = myDoubles.get(1)*canvas.getWidth();
-                    Double x2 = (myDoubles.get(2))*canvas.getWidth();
-                    Double y2 = (myDoubles.get(3)*canvas.getWidth());
-
-
-
-                    canvas.getGraphicsContext2D().fillRect(x1,y1,(x2-x1),(y2-y1));
-                    canvas.getGraphicsContext2D().strokeRect(x1,y1,(x2-x1),(y2-y1));
-
-                    myDoubles.clear();
-
-                } else if (str.contains("ELLIPSE")) {
-
-                    // Scans for Doubles
-                    myDoubles = new ArrayList<Double>();
-                    doubles = Pattern.compile(regexDoubles).matcher(str);
-
-                    while (doubles.find()) {
-                        double element = Double.parseDouble(doubles.group());
-                        myDoubles.add(element);
-                    }
-
-                    Double x1 = myDoubles.get(0) * canvas.getWidth();
-                    Double y1 = myDoubles.get(1) * canvas.getHeight();
-                    Double x2 = myDoubles.get(2) * canvas.getWidth();
-                    Double y2 = myDoubles.get(3) * canvas.getHeight();
-
-
-
-
-                    canvas.getGraphicsContext2D().fillOval(x1,y1,x2-x1,y2-y1);
-                    canvas.getGraphicsContext2D().strokeOval(x1,y1,x2-x1,y2-y1);
-
-                } else if (str.contains("POLYGON")) {
-                    //System.out.println("I am a poly");
-                }
+                Draw draw = new Draw(canvas, str); // Draw shapes
             }
             br.close();
         } catch (FileNotFoundException e) {
